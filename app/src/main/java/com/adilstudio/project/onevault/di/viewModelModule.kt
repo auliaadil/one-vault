628d409c57126bdb8c0cbab862f854915b36fc73
Muhammad Aulia Adil <muhammad.aulia@mekari.com>
2025-07-20 04:26:42 +0700

    Add FileVaultViewModel in modules

--- app/src/main/java/com/adilstudio/project/onevault/di/viewModelModule.kt
+++ app/src/main/java/com/adilstudio/project/onevault/di/viewModelModule.kt
@@ -2,6 +2,7 @@ package com.adilstudio.project.onevault.di
 
 import com.adilstudio.project.onevault.presentation.bill.BillTrackerViewModel
 import com.adilstudio.project.onevault.presentation.credential.PasswordManagerViewModel
+import com.adilstudio.project.onevault.presentation.filevault.FileVaultViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
@@ -9,4 +10,5 @@ val viewModelModule = module {
     // Example binding, add other ViewModels as you implement them
     viewModel { BillTrackerViewModel(get(), get()) }
     viewModel { PasswordManagerViewModel(get(), get()) }
+    viewModel { FileVaultViewModel() }
 }
