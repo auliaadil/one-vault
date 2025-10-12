aec38f0a4fd5be176fe2ba7001c962b7526c254f
Muhammad Aulia Adil <aulia.techstudio@gmail.com>
2025-10-12 17:59:45 +0700

    Adjust padding in AboutScreen

--- app/src/main/java/com/adilstudio/project/onevault/presentation/settings/AboutScreen.kt
+++ app/src/main/java/com/adilstudio/project/onevault/presentation/settings/AboutScreen.kt
@@ -1,13 +1,26 @@
 package com.adilstudio.project.onevault.presentation.settings
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -42,16 +55,12 @@ fun AboutScreen(
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacing_xxxl)))
-
             // App Icon
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = stringResource(R.string.app_name),
                 modifier = Modifier.size(dimensionResource(R.dimen.app_icon_size))
             )
-
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.spacing_xxl)))
 
             // App Name
             Text(
